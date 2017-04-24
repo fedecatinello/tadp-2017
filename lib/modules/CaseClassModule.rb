5546f3674f2044aa79fed050fbd3f6567cfcbd10
Nico <nico93f@gmail.com>
2017-04-24 19:06:26 -0300

    Funcion Constructora

--- lib/modules/CaseClassModule.rb
+++ lib/modules/CaseClassModule.rb
@@ -13,6 +13,25 @@ module CaseClassModule
 
     def case_class(builder, &block)
       Object.const_set(builder.get_class_name, builder.build(&block))
+
+      # Defino un metodo con el mismo nombre de la clase para poder llamarla directamente y hacer el new
+      # Funcion Constructora (1a)
+      define_singleton_method(builder.get_class_name, lambda { |*attrs|
+        klass = Object.const_get(builder.get_class_name)
+        instance = klass.new.dup # dup para que pierda el freeze
+        ivars = instance.instance_variables
+
+        if attrs.length > ivars.length
+          # TODO: Poner un error mas lindo
+          throw 'Cantidad de argumentos incorrecta, se esperaban máximo: ' + ivars.length.to_s
+        end
+
+        attrs.each_with_index { |attr, i|
+          instance.send('instance_variable_set', ivars[i].to_s, attr)
+        }
+
+        instance.freeze
+      })
     end
 
   end
